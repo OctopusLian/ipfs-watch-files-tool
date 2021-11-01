5d9f8e9d10d81364a5f7914a324c17e0131e6ddf
OctopusLian <zoctopus@qq.com>
2021-11-01 16:50:39 +0800

    ipfs watch file add is done

--- version2_fsnotify/main.go
+++ version2_fsnotify/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	shell "github.com/ipfs/go-ipfs-api"
 	log "github.com/sirupsen/logrus"
 )
 
-var FileCidMap map[string]string
+var FileCidMap map[string]string //var声明，不占用内存
 
 type Watch struct {
 	watch *fsnotify.Watcher
@@ -18,10 +21,12 @@ type Watch struct {
 
 //监控目录
 func (w *Watch) watchDir(dir string) {
-	FileCidMap = make(map[string]string)
+	//FileCidMap = make(map[string]string)
 
 	sh := shell.NewShell("localhost:5001")
 	log.Info("ipfs start")
+
+	c := context.Background()
 
 	//通过Walk来遍历目录下的所有子目录
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
@@ -52,19 +57,81 @@ func (w *Watch) watchDir(dir string) {
 						if err == nil && fi.IsDir() {
 							w.watch.Add(ev.Name)
 							fmt.Println("添加监控 : ", ev.Name)
+
+							//ipfs add dir
+							opt := func(builder *shell.RequestBuilder) error {
+								builder.Option("p", true)
+								return nil
+							}
+
+							err = sh.FilesMkdir(c, ev.Name, opt)
+							if err != nil {
+								log.Error("ipfs make dir failed: ", err)
+							}
+							log.Info("ipfs make dir success: ", ev.Name)
+						} else {
+							//ipfs add file
+							data, err := ioutil.ReadFile(ev.Name)
+							if err != nil {
+								log.Error("Read File error: %s", err)
+								continue
+							}
+							cid, err := sh.Add(strings.NewReader(string(data)))
+							if err != nil {
+								log.Error("ipfs add failed: ", err)
+							}
+							log.Info("ipfs add file:"+ev.Name+" cid is: ", cid)
+							//FileCidMap[ev.Name] = cid
 						}
+
 					}
 					if ev.Op&fsnotify.Write == fsnotify.Write {
 						fmt.Println("写入文件 : ", ev.Name)
+						data, err := ioutil.ReadFile(ev.Name)
+						if err != nil {
+							log.Error("Read File error: %s", err)
+							continue
+						}
+						// cid, err := sh.Add(strings.NewReader(string(data)))
+						// if err != nil {
+						// 	log.Error("ipfs add failed: ", err)
+						// }
+						opt := func(builder *shell.RequestBuilder) error {
+							builder.Option("p", true)
+							return nil
+						}
+						err = sh.FilesWrite(c, ev.Name, strings.NewReader(string(data)), opt) //TODO:如何获取命令行输入的内容
+						if err != nil {
+							log.Error("ipfs write file failed: ", ev.Name)
+							continue
+						}
+						log.Info("ipfs write file success:" + ev.Name)
+						//FileCidMap[ev.Name] = cid
 					}
 					if ev.Op&fsnotify.Remove == fsnotify.Remove {
 						fmt.Println("删除文件 : ", ev.Name)
 						//如果删除文件是目录，则移除监控
 						fi, err := os.Stat(ev.Name)
-						if err == nil && fi.IsDir() {
+						if err == nil && fi.IsDir() { // this name is dir
 							w.watch.Remove(ev.Name)
 							fmt.Println("删除监控 : ", ev.Name)
+							err = sh.FilesRm(c, ev.Name, true) //TODO:参数true代表删除目录是否递归？
+							if err != nil {
+								log.Error("ipfs rm dir failed: ", err)
+								continue
+							}
+							log.Info("ipfs rm dir success: ", ev.Name)
+						} else {
+							//是文件，就不需要移除监控，直接ipfs调用删除接口
+							err = sh.FilesRm(c, ev.Name, false)
+							if err != nil {
+								log.Error("ipfs rm file failed: ", err)
+								continue
+							}
+							log.Info("ipfs rm success: ", ev.Name)
 						}
+
+						//delete(FileCidMap, ev.Name)
 					}
 					if ev.Op&fsnotify.Rename == fsnotify.Rename {
 						fmt.Println("重命名文件 : ", ev.Name)
@@ -73,9 +140,16 @@ func (w *Watch) watchDir(dir string) {
 						//因为重命名后，go已经无法找到原文件来获取信息了
 						//所以这里就简单粗爆的直接remove好了
 						w.watch.Remove(ev.Name)
+						//ipfs rm
+						err := sh.FilesRm(c, ev.Name, true)
+						if err != nil {
+							log.Error("ipfs rm failed: ", err)
+						}
+						log.Info("ipfs rm success: ", ev.Name)
 					}
 					if ev.Op&fsnotify.Chmod == fsnotify.Chmod {
 						fmt.Println("修改权限 : ", ev.Name)
+
 					}
 				}
 			case err := <-w.watch.Errors:
@@ -95,7 +169,6 @@ func main() {
 
 	//FileCidMap := make(map[string]string)
 
-	//sh := shell.NewShell("localhost:5001")
 	log.Info("start")
 
 	watch, _ := fsnotify.NewWatcher()
